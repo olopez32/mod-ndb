05c0f0f955321936a48a1d03add93c11342a5c98
jdd <jdd@cf97b7ce-4e70-11de-a6a3-df26d2c71dbe>
2006-08-22 20:51:22 +0000

    Added mod_ndb_config.h

---
+++ trunk/mod_ndb_config.h
@@ -0,0 +1,66 @@
+/* Copyright (C) 2006 MySQL AB
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+*/
+
+
+namespace config {
+  
+  /* Apache per-server configuration  */
+  struct srv {
+    char *connect_string;
+  };
+  
+  
+  /* Apache per-directory configuration */
+  struct dir {
+    char *database;
+    char *table;
+    int allow_delete;
+    result_format results;
+    char *format_param[2];
+    array_header *visible;
+    array_header *updatable;
+    array_header *indexes;
+    array_header *key_columns;
+  };
+  
+  /* NDB Index */
+  class index {
+    public:
+      char *name;
+      ndb_key_type type;
+      short first_col;
+  };
+  
+  /* Coulmn used in a query */
+  class key {
+    public:
+      char *name;
+      ndb_key_type type;
+      NdbScanFilter::BinaryCondition op;
+      short next_in_key;
+  };
+  
+  void * init_dir(pool *, char *);
+  void * init_srv(pool *, server_rec *);
+  void * merge_dir(pool *, void *, void *);
+  const char * build_column_list(cmd_parms *, void *, char *);
+  const char * build_index_list(cmd_parms *, void *, char *, char *);
+  const char * set_result_format(cmd_parms *, void *, char *, char *, char *);
+  const char * pathinfo(cmd_parms *, void *, char *);
+  const char * filter(cmd_parms *, void *, char *, char *, char *);
+}
+
