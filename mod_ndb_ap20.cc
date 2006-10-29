7c2f4b54774cc5ce0f724ad229099cee505f09e0
jdd <jdd@cf97b7ce-4e70-11de-a6a3-df26d2c71dbe>
2006-10-29 07:43:36 +0000

    This version compiles and runs with Apache 2

--- mod_ndb_ap20.cc
+++ mod_ndb_ap20.cc
@@ -17,41 +17,42 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 #include "mod_ndb.h"
 
-
 /* Multi-threaded Apache 2 version: */
 struct mod_ndb_process process;
 int apache_is_threaded = 0;
 int ndb_force_send = 0;
 apr_status_t mod_ndb_child_exit(void *);
 
-
 //
 // INITIALIZATION & CLEAN-UP FUNCTIONS:
 //
-apr_status_t mod_ndb_child_init(ap_pool *p, ap_pool *plog, 
-                                ap_pool *ptmp, server_rec *s) {
-  /* Initialize the NDB API */
+void mod_ndb_child_init(ap_pool *p, server_rec *s) {
+  int n_ok = 0;
+  int n_fail = 0;
+
+    /* Initialize the NDB API */
   ndb_init();
 
   /* Get server configuration */
   config::srv *srv = (config::srv *)
     ap_get_module_config(s->module_config, &ndb_module);
 
-  /* Connections (distinct cluster connect strings) */
-  process.n_connections = 1;
-  connect_to_cluster(& process.conn, s, srv);
-
   /* Multi-threaded? */
   ap_mpm_query(AP_MPMQ_IS_THREADED, &apache_is_threaded);
+  
   if(apache_is_threaded) {
-    process.n_threads = 1;
-    process.thread_limit = 1;
-  }
-  else {
     ap_mpm_query(AP_MPMQ_MAX_THREADS, & process.n_threads);
     ap_mpm_query(AP_MPMQ_HARD_LIMIT_THREADS, & process.thread_limit);
   }
-
+  else {
+    process.n_threads = 1;
+    process.thread_limit = 1;
+  }
+  
+  /* Connections (distinct cluster connect strings) */
+  process.n_connections = 1;
+  connect_to_cluster(& process.conn, s, srv);
+    
   /* Allocate the global instance table */
   process.conn.instances = (ndb_instance **) ap_pcalloc(p, 
               (process.n_threads * sizeof(ndb_instance *)));
@@ -60,14 +61,22 @@ apr_status_t mod_ndb_child_init(ap_pool *p, ap_pool *plog,
     /* Create an instance */
     process.conn.instances[i] = 
       (ndb_instance *) ap_pcalloc(p, sizeof(ndb_instance));
-    init_instance(& process.conn , process.conn.instances[i]);
- } 
+    Ndb *ndbp = init_instance(& process.conn , process.conn.instances[i]);
+    if(ndbp) n_ok++;
+    else n_fail++;
+  }
+
+  ap_log_error(APLOG_MARK, log::err, 0, s, "Node %d: Initializing "
+               "NDB thread instances (%d succes%s, %d failure%s).", 
+               process.conn.connection->node_id(),
+               n_ok, (n_ok == 1 ? "": "es") ,
+               n_fail, (n_fail == 1 ? "" :  "s")); 
   
   /* Register the exit handler */
   apr_pool_cleanup_register(p, (const void *) s, 
                             mod_ndb_child_exit, mod_ndb_child_exit);
                             
-  return APR_SUCCESS;
+  return /* APR_SUCCESS */;
 }
 
 
@@ -76,23 +85,26 @@ apr_status_t mod_ndb_child_init(ap_pool *p, ap_pool *plog,
 //
 apr_status_t mod_ndb_child_exit(void *v) {
   server_rec *s = (server_rec *) v;
-  ndb_connection *c;
-  ndb_instance *i;
-  int n = 0, id = 0;
+  ndb_connection *c = & process.conn;
+  int id = 0;
+  int n_destroyed = 0;
   
-  for(c = & process.conn; c != (ndb_connection *) 0 ; c=c->next) {
-    if(c->connection != 0) {
-      id = c->connection->node_id();
+  if(c->connection != 0) {
+    id = c->connection->node_id();
       
-      /* These were allocated by the C++ runtime, so let C++ free them,
-         e.g. during "apachectl graceful"
-      */    
-      for(i = c->instances[n] ; n < process.n_threads ; n++)
-          delete i->db;
-      delete c->connection;
-
-      log_err2(s, "Node %d disconnected from cluster.", id);
+    /* These were allocated by the C++ runtime, so let C++ free them,
+        e.g. during "apachectl graceful"
+    */    
+    for(int n = 0; n < process.n_threads ; n++) {
+      ndb_instance *i = process.conn.instances[n];
+      if(i && i->db) {
+        delete i->db;
+        n_destroyed++;
+      }
     }
+    delete c->connection;
+    log_err3(s, "Node %d disconnected from cluster; destroyed %d Ndb instances.", 
+             id, n_destroyed);
   }
 
   /* Shut down the NDB API */
@@ -127,8 +139,12 @@ void connect_to_cluster(ndb_connection *c, server_rec *s, config::srv *srv) {
   /* Succesfully connected */
   c->connected=1;
   ap_log_error(APLOG_MARK, log::err, 0, s,
-               "Process %d connected to NDB Cluster as node %d", 
-               getpid(), c->connection->node_id());
+               "Process %d connected to NDB Cluster as node %d "
+               "(%d thread%s; hard limit: %d)", 
+               getpid(), c->connection->node_id(),
+               process.n_threads, 
+               process.n_threads == 1 ? "" : "s",
+               process.thread_limit);
 
   /* Some day this might be configurable */
   c->ndb_force_send = ndb_force_send;
@@ -137,18 +153,20 @@ void connect_to_cluster(ndb_connection *c, server_rec *s, config::srv *srv) {
 }
 
 
-void init_instance(ndb_connection *c, ndb_instance *i) {
+Ndb *init_instance(ndb_connection *c, ndb_instance *i) {
   
   /* The C++ runtime allocates an Ndb object here */
   i->db = new Ndb(c->connection);
-  
-  /* init(n) where n is max no. of active transactions; default is 4 */
-  i->db->init();
-  
+
+  if(i->db) {
+    /* init(n) where n is max no. of active transactions; default is 4 */
+    i->db->init();
+  }
+
   /* i->conn is a pointer back to the parent connection */
   i->conn = c;
 
-  return;
+  return i->db;
 }
 
 
@@ -283,19 +301,17 @@ namespace config {
   };
 }
 
-
-
-extern int ndb_handler(request_rec *);
-
 extern "C" {
 
+  extern int ndb_handler(request_rec *);
+  
  /************************
   *       Hooks          *
   ************************/
 
   void mod_ndb_register_hooks(ap_pool *p) {
     ap_hook_handler(ndb_handler, NULL, NULL, APR_HOOK_MIDDLE);
-    ap_hook_post_config(mod_ndb_child_init, NULL, NULL, APR_HOOK_MIDDLE);
+    ap_hook_child_init(mod_ndb_child_init, NULL, NULL, APR_HOOK_MIDDLE);
   }
     
   /************************
