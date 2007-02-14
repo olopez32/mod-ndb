9f4625a8b864fca78960855fd7dc2c5f9aa1fbc6
jdd <jdd@cf97b7ce-4e70-11de-a6a3-df26d2c71dbe>
2007-02-14 21:52:40 +0000

    batching: this checkin contains the bulk of the major changes related to batching.  Much of Query.cc was moved in to the new file Execute.cc, and much of struct QueryItems was moved to the ndb_instance or data_operation.

--- trunk/Query.cc
+++ trunk/Query.cc
@@ -1,4 +1,4 @@
-/* Copyright (C) 2006 MySQL AB
+/* Copyright (C) 2006, 2007 MySQL AB
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -17,22 +17,26 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 #include "mod_ndb.h"
 #include "ndb_api_compat.h"
-#include "util_md5.h"
 
 /* There are many varieties of query: 
    read, insert, update, and delete (e.g. HTTP GET, POST, and DELETE);
    single-row lookups and multi-row scans;
    access via primary key, unique hash index, ordered index, or scan filter;
-   queries that use blobs;
-   and final results returned in several different formats.
-   Each variant possibility is represented as a "module" in this file, 
-   and most (but not all) of those modules are functions that conform
-   to the "PlanMethod" typedef.
+   queries that use blobs.
+   Each variant possibility is represented as a module in this file, 
+   and those modules are functions that conform to the "PlanMethod" typedef.
 */
 typedef int PlanMethod(request_rec *, config::dir *, struct QueryItems *);
 
+/* The modules: */
+namespace Plan {
+  PlanMethod SetupRead; PlanMethod SetupWrite; PlanMethod SetupDelete; // setups
+  PlanMethod SetupInsert;
+  PlanMethod Read;      PlanMethod Write;      PlanMethod Delete;     // actions
+};  
 
-/* A runtime column is the runtime complement to the config::key_col structure
+
+/* A "runtime column" is the runtime complement to the config::key_col structure
 */
 class runtime_col {
   public:
@@ -45,7 +49,7 @@ class index_object;   // forward declaration
    which is used to pass essential data among the modules  
 */
 struct QueryItems {
-  Ndb *db;
+  ndb_instance *i;
   const NdbDictionary::Table *tab;
   const NdbDictionary::Index *idx;
   runtime_col *keys;
@@ -55,36 +59,14 @@ struct QueryItems {
   int n_filters;
   short *filter_list;
   AccessPlan plan;
-  table *form_data;
   PlanMethod *op_setup;
   PlanMethod *op_action;
-  PlanMethod *build_results;
-  result_buffer *results;
+  table *form_data;
   mvalue *set_vals;
+  data_operation *data;
 };  
 
 #include "index_object.h"
-
-
-/* Most modules are represented as PlanMethods
-*/
-namespace Plan {
- PlanMethod SetupRead; PlanMethod SetupWrite; PlanMethod SetupDelete; // setups
- PlanMethod SetupInsert;
- PlanMethod Read;      PlanMethod Write;      PlanMethod Delete;      // actions
-};  
-
-/*  Result formatters:
-*/
- PlanMethod Results_JSON;    PlanMethod Results_ap_note;
- PlanMethod Results_raw;     PlanMethod Results_XML;
- 
-
-/* This array corresponds to the four items in enum result_format_type:
-*/
-PlanMethod *result_formatter[5] = { 
-  0 , Results_JSON , Results_raw, Results_XML, Results_ap_note
-};
 
 
 /* Utility function declarations
@@ -97,9 +79,9 @@ short key_col_bin_search(char *, config::dir *);
 */
 int Plan::SetupRead(request_rec *r, config::dir *dir, struct QueryItems *q) {
   return q->plan == OrderedIndexScan ? 
-    q->scanop->readTuples(NdbOperation::LM_CommittedRead, 
+    q->data->scanop->readTuples(NdbOperation::LM_CommittedRead, 
                           dir->indexes->item(q->active_index).flag) :
-    q->op->readTuple(NdbOperation::LM_CommittedRead);
+    q->data->op->readTuple(NdbOperation::LM_CommittedRead);
 }
 
 int Plan::SetupInsert(request_rec *r, config::dir *dir, struct QueryItems *q) { 
@@ -111,7 +93,7 @@ int Plan::SetupWrite(request_rec *r, config::dir *dir, struct QueryItems *q) {
 }
 
 int Plan::SetupDelete(request_rec *r, config::dir *dir, struct QueryItems *q) { 
-  return q->op->deleteTuple(); 
+  return q->data->op->deleteTuple(); 
 }
 
 
@@ -173,18 +155,20 @@ inline void set_key(request_rec *r, short &n, char *value, config::dir *dir,
 
 
 /* Query():
-   Process an HTTP request,
-   formulate and run an NDB execution plan,
-   and create a results page.
+   Process an HTTP request, then formulate and run an NDB execution plan.
 */
 int Query(request_rec *r, config::dir *dir, ndb_instance *i) 
 {
   const NdbDictionary::Dictionary *dict;
-  result_buffer my_results;
-  my_results.buff = 0;
   struct QueryItems Q = 
-    { i->db, 0, 0, 0,0,0,0,0,0,0,0,0,
-      0, NoPlan, 0, Plan::SetupRead, Plan::Read, 0, &my_results, 0 };
+    { i, 0, 0,            // ndb_instance, tab, idx
+      0, 0, 0, 0,         // keys, active_index, idxobj, key_columns_used 
+      0, 0,               // n_filters, filter_list,
+      NoPlan,             // execution plan
+      Plan::SetupRead,    // setup module
+      Plan::Read,         // action module
+      0, 0, 0             // form_data, set_vals, data
+    };
   struct QueryItems *q = &Q;
   const NdbDictionary::Column *ndb_Column;
   bool keep_tx_open = false;
@@ -193,7 +177,13 @@ int Query(request_rec *r, config::dir *dir, ndb_instance *i)
   short col;
   int req_method = r->method_number;
   const char *subrequest_data = 0;
-      
+
+  /* Locate the appropriate data_operations object for this operation
+     and associate it with the directory config 
+  */
+  q->data = i->data + i->n_ops++;
+  q->data->dir = dir;
+  
   // Initialize all three of these, but only one will be needed: 
   PK_index_object       PK_idxobj(q,r);
   Unique_index_object   UI_idxobj(q,r);
@@ -202,8 +192,8 @@ int Query(request_rec *r, config::dir *dir, ndb_instance *i)
   // Initialize the data dictionary 
   i->db->setDatabaseName(dir->database);
   dict = i->db->getDictionary();
-  Q.tab = dict->getTable(dir->table);
-  if(Q.tab == 0) { 
+  q->tab = dict->getTable(dir->table);
+  if(q->tab == 0) { 
     log_err4(r->server, "mod_ndb could not find table %s (in database %s) "
              "in NDB Data Dictionary: %s",dir->table,dir->database,
              dict->getNdbError().message);
@@ -211,27 +201,22 @@ int Query(request_rec *r, config::dir *dir, ndb_instance *i)
     return NOT_FOUND; 
   }  
   
-  /* Initialize Q.keys, the runtime array of key columns which is used
+  /* Initialize q->keys, the runtime array of key columns which is used
      in parallel with the configure-time array dir->key_columns.   */
-  Q.keys = (runtime_col *) ap_pcalloc(r->pool, 
+  q->keys = (runtime_col *) ap_pcalloc(r->pool, 
            (dir->key_columns->size() * sizeof(runtime_col)));
 
   /* Is this request an Apache sub-request? 
   */
   if(r->main) {
-    const char *note = ap_table_get(r->main->notes,"ndb_keep_tx_open");
-    if(note && *note == '1') {
-      keep_tx_open = true;
-      ap_table_unset(r->main->notes,"ndb_keep_tx_open");
-    }
-    note = ap_table_get(r->main->notes,"ndb_request_method");
+    keep_tx_open = true;
+    const char *note = ap_table_get(r->main->notes,"ndb_request_method");
     if(note) {
       if(!strcmp(note,"POST")) req_method = M_POST;
       else if(!strcmp(note,"DELETE")) req_method = M_DELETE;
       ap_table_unset(r->main->notes,"ndb_request_method");
     }
     subrequest_data = ap_table_get(r->main->notes,"ndb_request_data");
-    /* To do: copy ndb_request_data and unset the note. */
   }
   
   /* Many elements of the query plan depend on the HTTP operation --
@@ -241,9 +226,8 @@ int Query(request_rec *r, config::dir *dir, ndb_instance *i)
     case M_GET:
       ap_discard_request_body(r);
       // Allocate an array of NdbRecAttrs for all desired columns 
-      Q.result_cols = (NdbRecAttr **) 
+      q->data->result_cols =  (const NdbRecAttr**)
         ap_pcalloc(r->pool, dir->visible->size() * sizeof(NdbRecAttr *));
-      Q.build_results = result_formatter[dir->results];
       break;
     case M_POST:
       Q.op_setup = Plan::SetupWrite;
@@ -251,7 +235,7 @@ int Query(request_rec *r, config::dir *dir, ndb_instance *i)
       Q.form_data = 0;
       Q.set_vals = (mvalue *) ap_pcalloc(r->pool, dir->updatable->size() * sizeof (mvalue));
       if(r->main && subrequest_data) {
-        /* This is a subrequest. */
+        /* The POST data is in the "ndb_request_data" note. */
         Q.form_data = ap_make_table(r->pool, 4);
         register const char *c = subrequest_data;
         char *key, *val;
@@ -261,6 +245,7 @@ int Query(request_rec *r, config::dir *dir, ndb_instance *i)
           ap_unescape_url(val);
           ap_table_merge(Q.form_data, key, val);
         }
+        ap_table_unset(r->main->notes,"ndb_request_data");
       }
       else {
         /* Fetch the update request from the client */
@@ -335,13 +320,15 @@ int Query(request_rec *r, config::dir *dir, ndb_instance *i)
     return NOT_FOUND;    /* no query */
   }
   
-  /* Open a transaction.
+  /* Open a transaction, if one is not already open.
      This creates an obligation to close it later, using tx->close().
-     If a non-null i->tx already exists, then the transaction is open
-     because of keep_tx_open in a previous request.
   */    
   if(i->tx == 0) {
-    if(!(i->tx = i->db->startTransaction())) {
+    if(i->flags.aborted) {
+      /* Transaction was already aborted */
+      return NOT_FOUND;
+    }
+    if(!(i->tx = i->db->startTransaction())) {  // To do: supply a hint
       log_err2(r->server,"db->startTransaction failed: %s",
                 i->db->getNdbError().message);
       return NOT_FOUND;
@@ -369,20 +356,13 @@ int Query(request_rec *r, config::dir *dir, ndb_instance *i)
   if(Q.active_index < 0) goto abort;
 
   // Get an NdbOperation 
-  q->op = q->idxobj->get_ndb_operation(i->tx);
+  q->data->op = q->idxobj->get_ndb_operation(i->tx);
 
   // Query setup, e.g. Plan::SetupRead calls op->readTuple() 
   if(Q.op_setup(r, dir, & Q)) { // returns 0 on success
     log_debug(r->server,"Returning 404 because Q.op_setup() failed: %s",
-              Q.op->getNdbError().message);
+              q->data->scanop->getNdbError().message);
     goto abort;
-  }
-
-
-  // Debug:
-  if(q->scanop) {
-    if(q->scanop->getSorted()) log_debug(r->server, "scan is sorted %s", "asc");
-    if(q->scanop->getDescending()) log_debug(r->server, "scan is sorted %s", "desc");
   }
 
   // Traverse the index parts and build the query
@@ -405,10 +385,10 @@ int Query(request_rec *r, config::dir *dir, ndb_instance *i)
     col = dir->key_columns->item(col).next_in_key;
     if(! q->idxobj->next_key_part()) break;
   }
-
+ 
   // Set filters
   if(Q.plan == OrderedIndexScan && Q.n_filters) {
-    NdbScanFilter filter(Q.op);
+    NdbScanFilter filter(q->data->op);
     filter.begin(NdbScanFilter::AND);
     for(int n = 0 ; n < Q.n_filters ; n++) {
       int m = Q.filter_list[n];
@@ -426,153 +406,22 @@ int Query(request_rec *r, config::dir *dir, ndb_instance *i)
   }
 
   // Perform the action; i.e. get the value of each column
-  response_code = Q.op_action(r, dir, &Q);
-  
-  if(response_code == OK) {
-    /* Execute the transaction.  tx->execute() returns 0 on success. */
-    /* Do not commit if you will be reading a blob. */
-    NdbTransaction::ExecType commit_tx = 
-      (dir->results == raw ? NdbTransaction::NoCommit : NdbTransaction::Commit);
-    
-    if(i->tx->execute(commit_tx, NdbTransaction::AbortOnError, 
-                      i->conn->ndb_force_send))
-    {        
-      log_debug(r->server,"Returning 410 because tx->execute failed: %s",
-                i->tx->getNdbError().message);
-      response_code = HTTP_GONE;
-    }
-    else 
-      if(Q.build_results)
-        response_code = Q.build_results(r, dir, q);
-  }
+  if(Q.op_action(r, dir, &Q)) 
+    goto abort;
 
-  // If this is not a subrequest, set some HTTP response headers
-  if(response_code == OK && (! r->main)) {   
-    // Set content-length
-    if(q->results->buff)
-      ap_set_content_length(r, q->results->sz);
-    else 
-      ap_set_content_length(r, 0);
-    
-    // Set ETag
-    if(dir->use_etags && q->results->buff) {
-      char *etag = ap_md5_binary(r->pool, (const unsigned char *) 
-                                 q->results->buff, q->results->sz);
-      ap_table_setn(r->headers_out, "ETag",  etag);
-      // If the ETag matches the client's cache, the page should not be returned 
-      response_code = ap_meets_conditions(r);
-    }
-    
-    ap_send_http_header(r);
+  if(keep_tx_open) 
+    return OK;
+  else
+    return ExecuteAll(r, i);
 
-    /* Send the page  (but recheck the response code,
-       after ap_meets_conditions) */   
-    if(response_code == OK && q->results->buff)
-      ap_rwrite(q->results->buff, q->results->sz, r);
-  }
-  
-  if(keep_tx_open) {
-    log_debug(r->server,"keeping tx %qu open.", i->tx->getTransactionId());
-  }
-  else {
-    i->tx->close();
-    i->tx = 0;
-  }
-  return response_code;
-  
   abort:
+  // Look at this later.  A failure of any operation causes the whole transaction
+  // to be aborted?  
   log_debug(r->server,"Aborting open transaction at '%s'",r->unparsed_uri);
   i->tx->close();
   i->tx = 0;
+  i->flags.aborted = 1;
   return NOT_FOUND;
-}
-
-
-
-/******** Result Page formatters *************/
-
-
-inline void JSON_send_result_row(request_rec *r, config::dir *dir, 
-                                 struct QueryItems *q) {
-  q->results->out(JSON::new_object);
-  for(int n = 0; n < dir->visible->size() ; n++) {
-    if(n) q->results->out(JSON::delimiter);
-    JSON::put_member(*q->results, *q->result_cols[n], r);
-  }
-  q->results->out(JSON::end_object);
-}
-  
-
-int Results_JSON(request_rec *r, config::dir *dir, struct QueryItems *q) {
-  int nrows = 0;
-  q->results->init(r, 8192);
-
-  if(q->scanop) {
-    /* Multi-row result set */   
-    while((q->scanop->nextResult(true)) == 0) {
-      do {
-        if(nrows++) q->results->out(",\n");
-        else q->results->out(JSON::new_array,r);
-        JSON_send_result_row(r, dir, q);
-      } while((q->scanop->nextResult(false)) == 0);
-    }
-    if(nrows) q->results->out(JSON::end_array);
-    else return HTTP_GONE;
-  }
-  else {
-    /* Single row result set */
-    JSON_send_result_row(r, dir, q);
-  }
-  
-  q->results->out("\n");
-  return OK;
-}
-
-
-int Results_raw(request_rec *r, config::dir *dir, struct QueryItems *q) {
-  unsigned long long size64 = 0;
-  unsigned int size;
-  
-  if(q->blob) {
-    q->blob->getLength(size64);  //passed by reference
-    size = (unsigned int) size64;
-    q->results->init(r, size);
-    if(q->blob->readData(q->results->buff, size)) 
-      log_debug(r->server,"Error reading blob data: %s",
-                q->blob->getNdbError().message);
-    q->results->sz = size;
-  }
-  return OK;
-}
-
-int Results_XML(request_rec *r, config::dir *dir, struct QueryItems *q) {
-  log_debug(r->server,"In Results formatter %s", "Results_XML");
-
-  return NOT_FOUND;
-}
-
-int Results_ap_note(request_rec *r, config::dir *dir, struct QueryItems *q) {
-  log_debug(r->server,"In Results formatter %s", "Results_ap_note");
-  // If this is a subrequest, set the note for the parent:
-  request_rec *rr = r->main ? r->main : r;
-  
-  if(dir->sub_results != no_results && dir->sub_results != ap_note) {
-    /* Generate results in the sub_result format (e.g. JSON or raw) 
-       and save them as a note named "NDB_results" 
-    */
-    result_formatter[dir->sub_results](r,dir,q); 
-    ap_table_set(rr->notes,"NDB_results",q->results->buff);
-  }
-  else {
-    for(int n = 0; n < dir->visible->size() ; n++) {
-      register NdbRecAttr *rec =  q->result_cols[n];
-      if(! rec->isNULL()) {
-        register const NdbDictionary::Column* col = rec->getColumn();
-        ap_table_set(rr->notes, col->getName(), MySQL::result(r->pool, *rec));
-      }
-    }
-  }
-  return OK;
 }
 
 
@@ -582,18 +431,19 @@ int Plan::Read(request_rec *r, config::dir *dir, struct QueryItems *q) {
   // Call op->getValue() for each desired result column
   column_list = dir->visible->items();
   for(int n = 0; n < dir->visible->size() ; n++) {
-    q->result_cols[n] = q->op->getValue(column_list[n], 0);
+    q->data->result_cols[n] = q->data->op->getValue(column_list[n], 0);
 
     /* If the result format is "raw", check for BLOBs */
     if(dir->results == raw) {
       int isz = q->tab->getColumn(column_list[n])->getInlineSize();
       if(isz) {   /* then the column is a blob... */
         log_debug(r->server,"Treating column %s as a blob",column_list[n])
-        q->blob = q->op->getBlobHandle(column_list[n]);
+        q->data->blob = q->data->op->getBlobHandle(column_list[n]);
+        q->i->flags.has_blob = 1;
       }
     }
   }
-  return OK;
+  return 0;
 }
 
 
@@ -623,59 +473,60 @@ int set_up_write(request_rec *r, config::dir *dir,
       else log_err2(r->server,"AllowUpdate list includes invalid column name %s", key);
     }
   }
-  if(is_insert) return q->op->insertTuple();
-  return is_interpreted ? q->op->interpretedUpdateTuple() : q->op->writeTuple();
+  if(is_insert) return q->data->op->insertTuple();
+  return is_interpreted ? q->data->op->interpretedUpdateTuple() : 
+                          q->data->op->writeTuple();
 }
 
 
 int Plan::Write(request_rec *r, config::dir *dir, struct QueryItems *q) {
   const NdbDictionary::Column *col;
+  int eqr;  
   
   // iterate over the mvalues that were set up in Plan::SetupWrite
   for(int n = 0; n < dir->updatable->size() ; n++) {
     mvalue &mval = q->set_vals[n];
     col = mval.ndb_column;
     if(col) {
-      int eqr;
       Uint64 next_value;
       if(mval_is_usable(r, mval)) {
         switch(mval.use_value) {
           case use_char:
-            eqr = q->op->setValue(col->getColumnNo(), mval.u.val_const_char );
+            eqr = q->data->op->setValue(col->getColumnNo(), mval.u.val_const_char );
             break;
           case use_autoinc:
             /* to do: tunable prefetch */
-            eqr = get_auto_inc_value(q->db, q->tab, next_value, 10);
+            eqr = get_auto_inc_value(q->i->db, q->tab, next_value, 10);
             if(!eqr) 
               eqr = (mval.len == 8 ?
-                 q->op->setValue(col->getColumnNo(), next_value) :
-                 q->op->setValue(col->getColumnNo(), (Uint32) next_value));
+                 q->data->op->setValue(col->getColumnNo(), next_value) :
+                 q->data->op->setValue(col->getColumnNo(), (Uint32) next_value));
             break;
           case use_null:
-            eqr = q->op->setValue(col->getColumnNo(), 0);
+            eqr = q->data->op->setValue(col->getColumnNo(), 0);
             break;
           case use_interpreted: 
             if(mval.interpreted == is_increment) 
-              eqr = q->op->incValue(col->getColumnNo(), (Uint32) 1);
+              eqr = q->data->op->incValue(col->getColumnNo(), (Uint32) 1);
             else if(mval.interpreted == is_decrement) 
-              eqr = q->op->subValue(col->getColumnNo(), (Uint32) 1);
+              eqr = q->data->op->subValue(col->getColumnNo(), (Uint32) 1);
             else assert(0);
             break;
           default:
-            eqr = q->op->setValue(col->getColumnNo(), (const char *) (&mval.u.val_char));
+            eqr = q->data->op->setValue(col->getColumnNo(), (const char *) (&mval.u.val_char));
         }
       } /* if(mval_is_usable) */
       else eqr = -1;
-      if(eqr) log_debug(r->server,"setValue failed: %s", q->op->getNdbError().message);
+      if(eqr) log_debug(r->server,"setValue failed: %s", q->data->op->getNdbError().message);
     }
   } // for()
-  return OK;
+  return eqr;
 }
 
 
 int Plan::Delete(request_rec *r, config::dir *dir, struct QueryItems *q) {
   log_debug(r->server,"Deleting Row %s","")
-  return OK;
+  return 0;
 }
 
 
