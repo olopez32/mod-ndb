7929c828baa2b13480a61af3cbff6570e496923e
jdd <jdd@cf97b7ce-4e70-11de-a6a3-df26d2c71dbe>
2007-02-15 03:34:55 +0000

    Cleanup of examples

--- trunk/htdocs/test_note.php
+++ trunk/htdocs/test_note.php
@@ -3,23 +3,14 @@
 
 <?php
 
-$req = "/ndb/as-note?channel=12";
+$req = "/ndb/table1?channel=12";
 
 if(virtual($req))
 	echo "virtual() success.<br>\n";
 else echo "virtual() failure.<br>\n";
 
 echo "<p>Subrequest: $req <br>";
-echo "NDB Results: " . apache_note("NDB_results") . "</br>\n";
-
-$req = "/ndb/one-row-note/1";
-echo "<P>Subrequest: $req <br>\n";
-virtual($req);  
-if(! apache_note("id")) 
-	echo "cannot fetch id note. <br>\n";
-echo "id: " . apache_note("id") . "<br>\n";
-echo "name: " . apache_note("name") . "<br>\n";
-echo "channel: " . apache_note("channel") . "<br>\n";
+echo "NDB Results: " . apache_note("ndb_result_0") . "</br>\n";
 
 echo "</html>\n";
 
