b8bfe0eeb714beaf8dd84b47e9ed247a8f891b1f
jdd <jdd@cf97b7ce-4e70-11de-a6a3-df26d2c71dbe>
2007-03-09 01:53:24 +0000

    Improvements to example1 (better comments; column aliases)

--- trunk/example1/t2_update_sess.sh
+++ trunk/example1/t2_update_sess.sh
@@ -5,4 +5,4 @@
 # Update the "last_page_visited" key for session 3
 
 curl -i -d 'sess_var_value=maps.php'  \
- "$BASE_URL/session?sess_id=3&sess_var_name=last_page_visited"
+ "$BASE_URL/session?id=3&var=last_page_visited"
--- trunk/example1/t6_delete_sess.sh
+++ trunk/example1/t6_delete_sess.sh
@@ -4,4 +4,4 @@
 
 # Delete a row of data 
 
-curl -i -X DELETE "$BASE_URL/session?sess_id=4&sess_var_name=user_name"
+curl -i -X DELETE "$BASE_URL/session?id=4&var=user_name"
