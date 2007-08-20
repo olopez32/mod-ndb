7ca3465824dd399af487dd63494e659f5692badb
jdd <jdd@cf97b7ce-4e70-11de-a6a3-df26d2c71dbe>
2007-08-20 05:51:59 +0000

    In request(), only return the results if the status is 200.

--- example-scripts/emp.js
+++ example-scripts/emp.js
@@ -2,25 +2,37 @@ var req = new XMLHttpRequest();
 var ids; // array of emperor ID's
 var idx = -1;   // index of the current emperor in the ids array
 
-function $(id)  {  return document.getElementById(id); }
-
-function load_ids() { return eval('(' + request("/demo/emperors") + ')'); }
-function next() {  if(idx+1 < ids.length) { idx++ ; get_emperor() } }
-function prev() {  if(idx > 0)            { idx-- ; get_emperor() } }
+function $(id)       { return document.getElementById(id); }
+function load_ids()  { return eval('(' + request("/demo/emperors") + ')'); }
+function next()      { if(idx+1 < ids.length) { idx++ ; get_emperor() } }
+function prev()      { if(idx > 0)            { idx-- ; get_emperor() } }
+function fmt_year(y) { return Math.abs(y) + (y < 0 ? " B.C." : ""); }
 
 function request(url) {
   req.open("GET",url,false);
   req.send(null);
-  return req.responseText;
-};
+  return (req.status == 200 ? req.responseText : "");
+}
 
 function get_emperor() {
-  var emp_id = ids[idx].id;
-  var i; 
-  $("photo").src = "/demo/photo/" + emp_id;
-  $("content").innerHTML = request("/demo/emperor/" + emp_id);
-  var com_list = eval('(' + request("/demo/comment?emp_id=" + emp_id) +')');
+  $("photo").src = "/demo/photo/" + ids[idx].id;
+  emp_obj = eval('(' + request("/demo/emperor/" + ids[idx].id) + ')');
+  var desc = "<b><big>" + emp_obj.name + "</big></b><br />" +
+             emp_obj.fullname + " " + fmt_year(emp_obj.start_year) +
+             " - " + fmt_year(emp_obj.end_year) + "<br />";
+  $("content").innerHTML = desc;
+  show_comments();
+}
+
+function show_comments() {
   $("comments").innerHTML = "";
-  for(i=0; i < com_list.length; i++) $("comments").innerHTML += 
-    "&bull; " + com_list[i].comment + "<br />\n";
-};
+  $("comments").innerHTML = request("/demo/comment?emp=" +ids[idx].id);
+}
+
+function add_comment() {
+  req.open("POST","/demo/comment",false);
+  req.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+  req.send("id=@autoinc&emp_id=" + ids[idx].id + "&comment=" + $("textbox").value);
+  $("textbox").value = "";
+  show_comments();
+}
