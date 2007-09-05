49bdf673c1b2c1294745ff78213e488802b932a6
jdd <jdd@cf97b7ce-4e70-11de-a6a3-df26d2c71dbe>
2007-09-05 02:25:21 +0000

    removed obsolete example1 (which has been merged into the test suite) and empty example3

--- trunk/example1/t2_update_sess.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-. ./base.test
-
-# Update the "last_page_visited" key for session 3
-
-curl -i -d 'sess_var_value=maps.php'  \
- "$BASE_URL/session?id=3&var=last_page_visited"
--- trunk/example1/t6_delete_sess.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-. ./base.test
-
-# Delete a row of data 
-
-curl -i -X DELETE "$BASE_URL/session?id=4&var=user_name"
