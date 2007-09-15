a58b538148ad771f58bd1b37d3c638dd4afc2db9
jdd <jdd@cf97b7ce-4e70-11de-a6a3-df26d2c71dbe>
2007-09-15 05:34:43 +0000

    merged from trunk

--- r1.0/Execute.cc
+++ r1.0/Execute.cc
@@ -19,6 +19,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "util_md5.h"
 #include "ndb_api_compat.h"
 
+
 inline void set_note(request_rec *r, int num, result_buffer &res) {
   char note[32];
   sprintf(note, "ndb_result_%d",num);
@@ -27,39 +28,62 @@ inline void set_note(request_rec *r, int num, result_buffer &res) {
 }
 
 
-/* handle_error_from_execute():
+inline void milliSleep(int milliseconds){
+  struct timeval sleeptime;
+  
+  sleeptime.tv_sec = milliseconds / 1000;
+  sleeptime.tv_usec = (milliseconds - (sleeptime.tv_sec * 1000)) * 1000000;
+  select(0, 0, 0, 0, &sleeptime);
+}
+
+
+/* handle_exec_error():
    set response code, and return 0 on pass-through or 1 on retry
 */
-bool handle_error_from_execute(request_rec *r, int &response_code, 
-                               const NdbError &error) {
-  bool do_log_error = 0;
-  bool retry_tx = 0;
+bool handle_exec_error(request_rec *r, int &response_code, 
+                               const NdbError &error) {  
+  bool stale_dictionary = 0;
   
+  // client errors
   if(error.classification == NdbError::NoDataFound)
     response_code = 404;
   else if(error.classification == NdbError::ConstraintViolation)
     response_code = 409;
-  else 
-    response_code = 400;  
-
-  log_debug(r->server,"tx->execute failed: %s", error.message);
-  if(do_log_error)
-    log_err(r->server,"tx->execute failed: %s %s", error.message, error.details);
-    
-  return retry_tx;
+  // server errors
+  else if(error.classification == NdbError::SchemaError) {
+    switch(error.code) {
+      case 241:  /* Invalid schema object version */
+      case 283:  /* Table is being dropped */
+      case 284:  /* Table not defined in transaction coordinator */
+      case 709:  /* No such table existed */
+        stale_dictionary = 1;
+        response_code = 500;
+        break;
+      default:
+        log_err(r->server,"tx execute failed: %s %s", error.message, error.details);
+        response_code = 500;
+    }
+  }
+  // misc. server error 
+  else {
+    response_code = 500;  
+    log_err(r->server,"tx execute failed: %s %s", error.message, error.details);
+  }
+  return stale_dictionary;
 }
 
 
-/******** Execute a batched transaction *************/
+/******** Execute batched transactions *************/
 
 int ExecuteAll(request_rec *r, ndb_instance *i) {
  
   int response_code = OK;
   int opn;     // operation number
-  int exec_err; 
+  unsigned int retries = 0, total_wait_time = 0;
+  bool apache_notes = 0, must_restart = 0;
+  char *error_message = 0;
   result_buffer my_results;
   my_results.buff = 0;
-  bool apache_notes = 0;
   
   log_debug(r->server, "Entering ExecuteAll() with %d read operations",
             i->n_read_ops);
@@ -89,10 +113,10 @@ int ExecuteAll(request_rec *r, ndb_instance *i) {
   */
   if(i->flag.has_blob) {
     /* Execute NoCommit */
-    if(i->tx->execute(NdbTransaction::NoCommit, TX_ABORT_OPT, 
-                      i->conn->ndb_force_send))
-      handle_error_from_execute(r, response_code, i->tx->getNdbError());
-
+    if(i->tx->execute(NdbTransaction::NoCommit)) {
+      must_restart = handle_exec_error(r, response_code,  i->tx->getNdbError());
+      goto cleanup1;
+    }
     /* Loop over operations & find BLOBs) */
     for(opn = 0 ; opn < i->n_read_ops ; opn++) {
       struct data_operation *data = i->data + opn ;
@@ -104,15 +128,25 @@ int ExecuteAll(request_rec *r, ndb_instance *i) {
   }
   
   /* Execute and Commit the transaction */
-  exec_err = i->tx->execute(NdbTransaction::Commit, TX_ABORT_OPT, 
-                                i->conn->ndb_force_send); 
-  /* After 5.1.15 you can get succesful execute() then NoDataFound */
-  if(exec_err || i->tx->getNdbError().classification == NdbError::NoDataFound) {
-    handle_error_from_execute(r, response_code, i->tx->getNdbError());
-    goto cleanup1;
-  } 
-  
+  exec_commit:
+  i->tx->execute(NdbTransaction::Commit, TX_ABORT_OPT, i->conn->ndb_force_send); 
 
+  if(i->tx->getNdbError().status == NdbError::TemporaryError) {
+    register unsigned int sleep_ms = 5 + ( 2 * retries * retries);
+    if(total_wait_time + sleep_ms < i->server_config->max_retry_ms) {
+      milliSleep(sleep_ms);  
+      total_wait_time += sleep_ms;
+      retries++;
+      goto exec_commit;
+    }  /* else: */
+    response_code = 503;
+    goto cleanup1;
+  }
+
+  if(i->tx->getNdbError().classification != NdbError::NoError) {
+    must_restart = handle_exec_error(r, response_code, i->tx->getNdbError());
+    goto cleanup1;
+  }
   
   /* Loop over the operations and build the result page */
   for(opn = 0 ; opn < i->n_read_ops ; opn++) {
@@ -151,9 +185,13 @@ int ExecuteAll(request_rec *r, ndb_instance *i) {
   }
 
   cleanup1:
-  if(response_code > 399) 
-    response_code = ndb_handle_error(r, response_code, & i->tx->getNdbError(), 0); 
-
+  if(must_restart && i->server_config->force_restart) {
+    response_code = ndb_handle_error(r, 503, & i->tx->getNdbError(), "10");
+    module_must_restart();
+  }
+  else if(response_code > 399) 
+    response_code = ndb_handle_error(r, response_code, 
+                                     & i->tx->getNdbError(), error_message); 
   i->tx->close();
   i->tx = 0;  
   
@@ -161,7 +199,8 @@ int ExecuteAll(request_rec *r, ndb_instance *i) {
   /* Clear all used operations */
   i->cleanup();
   
-  log_debug(r->server,"ExecuteAll() returning %d",response_code);    
+  log_debug(r->server, "ExecuteAll() returning %d%s", response_code,
+            (response_code == DONE) ? " (DONE)":"");    
   return response_code;
 }
 
--- r1.0/handlers.cc
+++ r1.0/handlers.cc
@@ -108,100 +108,6 @@ extern "C" {
   }
 
 
-  int ndb_config_check_handler(request_rec *r) {
-    table *param_tab;
-    config::key_col *columns;
-    
-    // Apache 2 Handler name check
-    CheckHandler(r,"ndb-config-check");
-
-    ndb_instance *i = my_instance(r);
-    
-    config::dir *dir = (config::dir *)
-      ap_get_module_config(r->per_dir_config, &ndb_module);
-    config::srv *srv = (config::srv *)
-      ap_get_module_config(r->server->module_config, &ndb_module);
-    
-    r->content_type = "text/plain";
-    ap_send_http_header(r);
-    
-    if(! (dir->database && dir->table)) {
-      ap_rprintf(r,"No database or table configured at %s\n",r->uri);
-      return OK;
-    }
-    
-    ap_rprintf(r,"Process ID: %d\n",getpid());
-    ap_rprintf(r,"Connect string: %s\n",srv->connect_string);
-    ap_rprintf(r,"Database: %s\n",dir->database);
-    ap_rprintf(r,"Table: %s\n",dir->table);
-    ap_rprintf(r,"Size of configuration structures:");
-    /* gcc may give a warning here where glibc printf uses "%z" for size_t */
-    ap_rprintf(r,"   dir: %lu.  index: %lu.  key_col: %lu.\n", sizeof(config::dir),
-               sizeof(config::index), sizeof(config::key_col));
-    if(i == (ndb_instance *) 0) {
-      ap_rprintf(r, "Cannot access NDB instance. ");
-      ap_rprintf(r, process.conn.connected ? "\n" : "Not connected to cluster.\n");
-    }
-    else {
-      i->db->setDatabaseName(dir->database);
-      NdbDictionary::Dictionary *dict = i->db->getDictionary();
-      const NdbDictionary::Table *tab = dict->getTable(dir->table);
-      ap_rprintf(r,"Node Id: %d\n",i->conn->connection->node_id());
-      ap_rprintf(r,"\n");
-      if(tab) {
-        ap_rprintf(r,"Primary key according to NDB Dictionary:");
-        for(int n = 0; n < tab->getNoOfPrimaryKeys() ; n++) 
-          ap_rprintf(r,"%s %s", (n ? "," : " ") , tab->getPrimaryKey(n));
-        ap_rprintf(r,"\n");
-      }
-      else
-        if(i) ap_rprintf(r," ** Table does not exist in data dictionary.\n");
-    }
-    if(dir->visible) {
-      ap_rprintf(r,"%d visible column%s:  ", dir->visible->size(),
-                 dir->visible->size() == 1 ? "" : "s");
-      ap_rprintf(r,"%s \n",ap_array_pstrcat(r->pool,dir->visible,',')); 
-    } 
-    if(dir->updatable) {
-      ap_rprintf(r,"%d updatable column%s:  ", dir->updatable->size(),
-                 dir->visible->size() == 1 ? "" : "s");
-      ap_rprintf(r,"%s \n",ap_array_pstrcat(r->pool,dir->updatable,','));  
-    }
-    ap_rprintf(r,"Result format: %s\n", dir->fmt->name);
-    
-    columns = dir->key_columns->items();
-    int n_indexes = dir->indexes->size();
-    int n_kcols = dir->key_columns->size();
-    ap_rprintf(r,"\n%d key column%s:    ",n_kcols, n_kcols == 1 ? "" : "s");
-    for(int n = 0 ; n < n_kcols ; n++)
-      ap_rprintf(r,"%s ",columns[n].name);
-    ap_rprintf(r,"\n%d index%s\n", n_indexes, n_indexes == 1 ? "" : "es");
-    for(int n = 0 ; n < n_indexes ; n++) {
-      config::index &idx = dir->indexes->item(n);
-      ap_rprintf(r,"Type: %c | %-30s\n",idx.type, idx.name);
-      int t = idx.first_col;
-      ap_rprintf(r," %d key column alias%s:", idx.n_columns,
-                 idx.n_columns == 1 ? "" : "es");
-      while(t != -1) {
-        ap_rprintf(r," %s", columns[t].name);
-        t = columns[t].next_in_key;
-      }
-      ap_rprintf(r,"\n");
-    }
-    ap_rprintf(r,"Pathinfo: ");
-    for(int n = 0 ; n < dir->pathinfo_size ; n++)
-      ap_rprintf(r,"%s ", columns[dir->pathinfo[n]].name);
-    
-    ap_rprintf(r,"\n\n");
-    ap_rprintf(r,"args: %s\n",r->args);
-    if(r->args) {
-      param_tab = http_param_table(r, r->args);
-      ap_table_do(print_all_params,r,param_tab,NULL);
-    }
-    return OK;
-  }
-
-
   int ndb_status_handler(request_rec *r) {
     
     // Apache 2 Handler name check
@@ -213,10 +119,14 @@ extern "C" {
     r->content_type = "text/plain";
     ap_send_http_header(r);
     
-    ap_rprintf(r,"Process ID: %d\n",getpid());
-    ap_rprintf(r,"Connect string: %s\n",srv->connect_string);
-    ap_rprintf(r,"NDB Cluster Connections: %d\n",process.n_connections);
-    ap_rprintf(r,"Apache Threads: %d\n",process.n_threads);
+    ap_rprintf(r, "Process ID: %d\n", getpid());
+    ap_rprintf(r, "Connect string: %s\n", srv->connect_string);
+    ap_rprintf(r, "NDB Cluster Connections: %d\n", process.n_connections);
+    ap_rprintf(r, "Apache Threads: %d\n", process.n_threads);
+    ap_rprintf(r, "Force restart on stale dictionary: %s\n",  
+               srv->force_restart ? "Yes" : "No");
+    ap_rprintf(r, "Max retry time on temporary errors: %d ms\n", srv->max_retry_ms);
+
     ndb_instance *i = my_instance(r);
     if(i == (ndb_instance *) 0) {
       ap_rprintf(r,"\n -- RUNTIME ERROR: Cannot retrieve an NDB instance.\n");
@@ -245,8 +155,9 @@ int ndb_handle_error(request_rec *r, int status,
   r->status = status;
   r->content_type = "text/plain";
   
-  if(status == 405) ap_table_setn(r->headers_out, "Allow", msg);
-  
+  if(status == 405 && msg) ap_table_setn(r->headers_out, "Allow", msg);
+  if(status == 503 && msg) ap_table_setn(r->headers_out, "Retry-After", msg);
+
   switch(status) {
     case 404:
       page.out(msg ? msg : "No data could be found.\n");
@@ -258,6 +169,8 @@ int ndb_handle_error(request_rec *r, int status,
       break;
     case 500:
       if(msg) page.out(msg);
+      break;
+    case 503:
       break;
     default:
       page.out("HTTP return code %d.\n", status);
@@ -297,9 +210,9 @@ const char * allowed_methods(request_rec *r, config::dir *dir) {
   apache_array<char *> *methods = new(r->pool, 4) apache_array<char *>;
 
   if(dir->visible->size())    *methods->new_item() = "GET";
-  if(dir->use_etags)          *methods->new_item() = "HEAD";
+  if(dir->flag.use_etags)     *methods->new_item() = "HEAD";
   if(dir->updatable->size())  *methods->new_item() = "POST";
-  if(dir->allow_delete)       *methods->new_item() = "DELETE";
+  if(dir->flag.allow_delete)  *methods->new_item() = "DELETE";
   
   return ap_array_pstrcat(r->pool, methods, ',');
 }
