4900805a958079518acc59fb252b15ce0de08b15
john.david.duncan@gmail.com <john.david.duncan@gmail.com@cf97b7ce-4e70-11de-a6a3-df26d2c71dbe>
2009-08-19 03:53:21 +0000

    Added copyright notice

--- sql/NSQL/Visitors/SemanticCheck.cpp
+++ sql/NSQL/Visitors/SemanticCheck.cpp
@@ -4,6 +4,25 @@
  *  Created on: Jul 19, 2009
  *      Author: tulay
  */
+
+/* Copyright (C) 2009 Sun Microsystems
+ All rights reserved. Use is subject to license terms.
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+ */
+
 /* refman-5.1-en.pdf p959
  * A PRIMARY KEY is a unique index where all key columns must be defined as NOT NULL. If they are not explicitly declared as
  * NOT NULL, MySQL declares them so implicitly (and silently). A table can have only one PRIMARY KEY. If you do not have a
